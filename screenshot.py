09ecebb195b3b8639c2f9870b9cdb36e9415851a
MooGHen <107130889+MooGHen@users.noreply.github.com>
2022-06-16 05:58:06 +0700

    [skip ci]

--- screenshot.py
+++ screenshot.py
@@ -6,7 +6,7 @@ screenshot.save('sd.png')
 yahhh = (random.randint(50,90))
 for waktu in range(1, 100):
     print(waktu)
-    time.sleep(0.1)
+    time.sleep(1)
     
     if waktu == yahhh:
         print('end')
