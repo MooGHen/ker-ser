6207d3ea568236c3601998c6cb2e30a641e1655d
MooGHen <107130889+MooGHen@users.noreply.github.com>
2022-06-16 05:57:50 +0700

    [skip ci]

--- screenshot.py
+++ screenshot.py
@@ -1,3 +1,15 @@
 import pyautogui
+import time
+import random
 screenshot = pyautogui.screenshot()
 screenshot.save('sd.png')
+yahhh = (random.randint(50,90))
+for waktu in range(1, 100):
+    print(waktu)
+    time.sleep(0.1)
+    
+    if waktu == yahhh:
+        print('end')
+        screenshot = pyautogui.screenshot()
+        screenshot.save('waktuend.png')
+        exit(0)
