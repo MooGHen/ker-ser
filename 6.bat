a8020b43d88087e58885d1f092aa5208e1b2883a
MooGHen <107130889+MooGHen@users.noreply.github.com>
2022-06-16 06:08:12 +0700

    [skip ci]

---
+++ 6.bat
@@ -0,0 +1,6 @@
+set mus=%random%%random%%random%%random%
+echo %mus%
+start "" chrome.exe --profile-directory="%mus%" https://chrome.google.com/webstore/detail/youtube-ad-auto-skipper/lokpenepehfdekijkebhpnpcjjpngpnd
+python test.py
+start "" chrome.exe --profile-directory="%mus%" https://youtu.be/zEnefMkh3js
+python startvideo.py
