b527ffbef2a5a28779418ff0a6dcf67f32e0faf1
MooGHen <107130889+MooGHen@users.noreply.github.com>
2022-06-16 02:25:49 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,1 +1,1 @@
-start "" chrome.exe --profile-directory="%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%" --app="data:text/html,<html><body><script>window.moveTo(800,640);window.resizeTo(300,200);window.location='https://youtu.be/-2ajSaGXf0Q';</script></body></html>"
+start "" chrome.exe --profile-directory="%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%" https://youtu.be/XHf3n52uyOI --proxy-server="127.0.0.1:%RANDOM%"
